c2426a963eb1d9060ded53aebe07781819c931d2
Name Ping Louie <namepinglouie@gmail.com>
2022-05-12 12:36:46 -0400

    completed entry controller and queries for table 2

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -5,7 +5,7 @@ INSERT INTO users (fname, lname, email, password) VALUES
 ('Billy','Jean','billyjean@hotmail.com','password1234'),
 ('Joey', 'Dean', 'jdbikes@aol.com', 'h0gwarts');
 
-INSERT INTO entries (userId, date, mood, interest, activity) VALUES
+INSERT INTO entries (userid, date_created, mood, interest, activity) VALUES
 (1, '05/11/2022 12:08:00', 'happy', 'animals, food, retail', 'cow therapy'),
 (2, '02/14/2022 14:14:00', 'sad', 'social, animals', 'hug therapy'),
 (3, '06/10/2022 18:00:00', 'excited', 'social, exercise','zumba'),
