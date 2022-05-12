0a9fc03df124c2a79f357f310c697abaabce7ff2
rob <robwalker@pursuit.org>
2022-05-12 16:22:10 -0400

    added navbar and home

---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+
+
+export default function NavBar () {
+    return (
+        <nav><Link to="/"><img src="https://dummyimage.com/200x200/00b3ff/0011ff.png" alt="Logo"/></Link> 
+            <Link to="/"><button>eMoodji</button></Link>
+        </nav>
+    
+        
+    )
+};
---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="Home">
+            <h1>Home Page</h1>
+        </div>
+    )
+};
