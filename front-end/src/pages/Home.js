99a5145fc199de138123735e504846ccb0513963
Jerry De Jesus <jdejesus7@live.com>
2022-05-18 11:16:07 -0400

    added a couple buttons to home page

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <div className="Home">
             <h1>Home Page</h1>
+            <h1>Description</h1>
+            <Link to="/entries/new"><button> New Entry </button></Link>
+            <button> Log In </button>
+            <Link to="/createaccount"><button> Create Account </button></Link>
         </div>
     )
 };
