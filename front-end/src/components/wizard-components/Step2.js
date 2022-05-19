ad77b7602ded3a12890cacbaf2faf34ce455ff7b
Name Ping Louie <namepinglouie@gmail.com>
2022-05-18 20:37:39 -0400

    working on step wizard

---
+++ front-end/src/components/wizard-components/Step2.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Step2(props) {
+    const {entry, handleText, next, back} = props;
+    return(
+        <form>
+            <div>
+                <label htmlFor="interest">Tell me what's your interest</label>
+                <input type = "text" id = "interest" value={entry.interest ?? ""} onChange={handleText} />
+                <button type = "button" onClick={back}>Back</button> 
+                <button type = "button" onClick={next}>Next</button>
+            </div>
+        </form>
+    )
+}
---
+++ front-end/src/components/wizard-components/Step4.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Step4(props) {
+    const {entry, handleText, next, back} = props;
+    return(
+        <form>
+            <div>
+                <label htmlFor="result">Result</label>
+                <input type = "text" id = "result" value={entry.result ?? ""} onChange={handleText} />
+                <button type = "button" onClick={back}>Back</button> 
+                <button type = "button" onClick={next}>Next</button>
+            </div>
+        </form>
+    )
+}
