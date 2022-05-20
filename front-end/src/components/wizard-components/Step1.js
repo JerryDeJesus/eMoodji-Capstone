4a51bf20862a87b9bc6d9d073dc84b88d4e3739f
Name Ping Louie <namepinglouie@gmail.com>
2022-05-20 16:02:41 -0400

    wizard step1 functionality fixed

--- front-end/src/components/wizard-components/Step1.js
+++ front-end/src/components/wizard-components/Step1.js
@@ -4,68 +4,53 @@ import axios from "axios";
 const ACCESS_KEY = process.env.REACT_APP_API_KEY;
 
 export default function Step1(props) {
-    const {entry, handleText, next} = props;
-    //const [emojis, setEmojis] = useState([]);
-    const [selectedEmojis, setSelectedEmojis] = useState([]);
-
-    // let searchEmojisAPI = `https://emoji-api.com/emojis?search=${entry.mood}&access_key=${ACCESS_KEY}`;
-
-    // useEffect (() => {
-    //     axios(`https://emoji-api.com/emojis?access_key=${ACCESS_KEY}`)
-    //         .then(res => {
-    //             console.log(res.data)
-    //             setEmojis(res.data);
-    //         })
-    //         .catch(error => console.log(error))
-    // }, []);
-
-    // const requestSearchEmojisAPI = axios.get(searchEmojisAPI);
-    // requestSearchEmojisAPI.then(res => setSelectedEmojis(res.data))
-    //                       .catch(error => console.log(error))
-
-    // const displayEmojis = emojis.filter((el, i) => i < 10).map((el, i) => {
-    //     return(
-    //         <li key = {i}>
-    //             {el.character}
-    //         </li>
-    //     )
-    // });
+    const {entry, setEntry, next} = props;
+    const [emojis, setEmojis] = useState([]);
+    const [moodInput, setMoodInput] = useState("");
 
     useEffect (() => {
-        axios(`https://emoji-api.com/emojis?search=${entry.mood}&access_key=${ACCESS_KEY}`)
+        axios(`https://emoji-api.com/emojis?access_key=${ACCESS_KEY}`)
             .then(res => {
-                setSelectedEmojis(res.data);
-                //console.log(res.data)
+                console.log(res.data)
+                setEmojis(res.data);
             })
             .catch(error => console.log(error))
-    }, [entry.mood]);
+        
+    }, []);
 
-    const displaySelectedEmojis = selectedEmojis.filter((el, i) => i < 10).map((el, i) =>{
-        return(
-            <li key = {i} style = {{fontSize: "40px"}}>{el.character}</li>
-        )
-    })
-
-    /*
-    // Need to Fix:
-    const replaceSearchedText = (moodText) => {
-        for(let e of selectedEmojis) {
-            moodText = entry.mood;
-            return entry.mood = e.slug;
-        }
+    const handleSelectEmoji = (emoji) => {
+        setEntry({...entry, mood : emoji.character});
+        console.log(emoji);
     }
 
-    console.log(replaceSearchedText(entry.mood));
-    */
+    const handleMoodInput = (e) => {
+        setMoodInput(e.target.value);
+    }
+
+    const filteredEmojis = emojis.filter((el, i) => {
+        if(i > 10 && moodInput.length === 0) {
+            return false;
+        } else {
+            return el.unicodeName.includes(moodInput);
+        }
+    })
+
+    const renderedFilteredEmojis = filteredEmojis.map((el, i) => {
+        return(
+            <li key = {i} onClick = {()=>handleSelectEmoji(el)}>
+                {el.character}
+            </li>
+        )
+    })
 
     return(
         <form>
             <div>
                 <h2>Hi, There!</h2>
                 <label htmlFor="mood">What's your emoodji for this moment?</label>
-                <input type="text" id = "mood" value = {entry.mood ?? ""} onChange = {handleText} />
+                <input type="text" id = "mood" value = {moodInput} onChange = {handleMoodInput} />
                 <br />
-                {displaySelectedEmojis}
+                {renderedFilteredEmojis}
                 <button type = "button" onClick={next}>Next</button>
             </div>
         </form>
