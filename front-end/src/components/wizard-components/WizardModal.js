5d728a3b338de5c25bb96fe6c7524deff70ea009
Jenna Qian <jiayin1996@gmail.com>
2022-05-24 15:53:34 -0400

    committing to pull down changes

--- front-end/src/components/wizard-components/WizardModal.js
+++ front-end/src/components/wizard-components/WizardModal.js
@@ -1,35 +1,97 @@
-import React, { useState } from "react";
-import {Modal} from "react-bootstrap";
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal, Button} from 'react-bootstrap';
 
-export default function WizardModal() {
-  const { show, setShow } = useState(false);
 
-  const handleHide = () => setShow(false);
-
+export default function InfoModal() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-    <Modal show={show} onHide={handleHide}>
-        <button onClick={handleShow}>BUTTTONNN</button>
+      <Button className="nextButton" onClick={handleShow}>
+        Open Modal
+      </Button>
 
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-     
-      <Modal.Footer>
-        <button onClick={handleHide}>
-          Close
-        </button>
-        <button onClick={handleHide}>
-          Save Changes
-        </button>
-      </Modal.Footer>
-    </Modal>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
+
+
+// import React, { useState } from "react";
+// import {Modal, Button} from "react-bootstrap";
+
+// export default function WizardModal() {
+//     const [show, setShow] = useState(false);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+//   return (
+   
+// //   const { show, setShow } = useState(false);
+
+// //   const handleHide = () => setShow(false);
+
+// //   const handleShow = () => setShow(true);
+
+// //   return (
+
+// //     //   <>
+// //     //     <Modal.Dialog>
+// //     //         <Modal.Header closeButton>
+// //     //             <Modal.Title>Modal title</Modal.Title>
+// //     //         </Modal.Header>
+
+// //     //         <Modal.Body>
+// //     //             <p>Modal body text goes here.</p>
+// //     //         </Modal.Body>
+
+// //     //         <Modal.Footer>
+// //     //             <Button variant="secondary">Close</Button>
+// //     //             <Button variant="primary">Save changes</Button>
+// //     //         </Modal.Footer>
+// //     //     </Modal.Dialog>
+// //     //   </>
+
+
+
+// //     // <>
+// //     // <Modal show={show} onHide={handleHide}>
+// //     //     <button onClick={handleShow}>BUTTTONNN</button>
+
+// //     //   <Modal.Header closeButton>
+// //     //     <Modal.Title>Modal heading</Modal.Title>
+// //     //   </Modal.Header>
+
+// //     //   <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+     
+// //     //   <Modal.Footer>
+// //     //     <button onClick={handleHide}>
+// //     //       Close
+// //     //     </button>
+// //     //     <button onClick={handleHide}>
+// //     //       Save Changes
+// //     //     </button>
+// //     //   </Modal.Footer>
+// //     // </Modal>
+// //     // </>
+// //   );
+// }
 
 
