4b83a71670f58e8fc825b02c527ca5ffb5e4a548
Name Ping Louie <namepinglouie@gmail.com>
2022-05-24 15:50:42 -0400

    working on modal for login and sign up

--- front-end/src/components/wizard-components/WizardModal.js
+++ front-end/src/components/wizard-components/WizardModal.js
@@ -1,35 +1,33 @@
 import React, { useState } from "react";
-import {Modal} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import { Link } from "react-router-dom";
 
 export default function WizardModal() {
-  const { show, setShow } = useState(false);
-
+  const [show, setShow] = useState(false);
   const handleHide = () => setShow(false);
-
   const handleShow = () => setShow(true);
 
   return (
     <>
-    <Modal show={show} onHide={handleHide}>
-        <button onClick={handleShow}>BUTTTONNN</button>
+    <button className="testing" onClick={handleShow}>SHOW BUTTTONNN</button>
+      <Modal show={show} onHide={handleHide} backdrop = "true">
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
 
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-     
-      <Modal.Footer>
-        <button onClick={handleHide}>
-          Close
-        </button>
-        <button onClick={handleHide}>
-          Save Changes
-        </button>
-      </Modal.Footer>
-    </Modal>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      
+        <Modal.Footer>
+        <Link to="/createaccount"><button onClick={handleHide}>
+            Create Account
+          </button></Link>
+          <Link to="/loginpage"><button onClick={handleHide}>
+            Log In
+          </button></Link>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
-
 
