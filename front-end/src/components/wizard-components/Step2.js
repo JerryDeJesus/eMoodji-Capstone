4619d651077b1e081e006f4f299d48bd767680ac
Name Ping Louie <namepinglouie@gmail.com>
2022-05-28 06:49:02 -0400

    working on wizard step2 border-radius

--- front-end/src/components/wizard-components/Step2.js
+++ front-end/src/components/wizard-components/Step2.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import hugIcon from "../../assets/icon-interests/hug.png"
 import massageIcon from "../../assets/icon-interests/massage.png"
 import musicIcon from "../../assets/icon-interests/music.png"
@@ -7,9 +8,21 @@ import yogaIcon from "../../assets/icon-interests/yoga.png"
 
 export default function Step2(props) {
     const {progressBarComponent, entry, setEntry, next, back} = props;
+    const [isActiveClick, setIsActiveClick] = useState(false);
     
     const handleSelectInterest = (e) => {
-        setEntry({...entry, interest: e})
+        if(!isActiveClick) {
+            e.target.style.border = "8px solid white";
+            e.target.style.borderRadius = "50px";
+            setEntry({...entry, interest: e.target.alt});
+            setIsActiveClick(!isActiveClick);
+        } else {
+            e.target.style.border = "none";
+            e.target.style.borderRadius = "none";
+            setEntry({...entry, interest: ""});
+            setIsActiveClick(!isActiveClick);
+        }
+
     }
 
     return(
@@ -22,12 +35,12 @@ export default function Step2(props) {
                 <p className="wizard-question">Tell me, what's your interest?</p>
 
                 <div className="interest-container">
-                    <img src={hugIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e.target.alt)} alt="hug"/>
-                    <img src={massageIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e.target.alt)} alt="acupuncture"/>
-                    <img src={musicIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e.target.alt)} alt="music"/>
-                    <img src={paintIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e.target.alt)} alt="art"/>
-                    <img src={petsIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e.target.alt)} alt="pet"/>
-                    <img src={yogaIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e.target.alt)} alt="yoga"/>
+                    <img src={hugIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="hug" className="interest-hover" />
+                    <img src={massageIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="acupuncture" className="interest-hover" />
+                    <img src={musicIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="music" className="interest-hover" />
+                    <img src={paintIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="art" className="interest-hover" />
+                    <img src={petsIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="pet" className="interest-hover" />
+                    <img src={yogaIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="yoga" className="interest-hover" />
                 </div>
                 
                 <div className="button-container step">
--- front-end/src/components/wizard-components/StepSubmit.js
+++ front-end/src/components/wizard-components/StepSubmit.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import WizardModal from "./WizardModal.js";
+import Flippy, {FrontSide, BackSide} from "react-flippy";
+import React from "react";
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -9,18 +10,74 @@ export default function StepSubmit(props) {
     const {entry} = props;
     const navigate = useNavigate();
     const userEntriesLink = `/users/${localStorage.getItem('userid')}/entries`;
+
+    const cardStyle = {
+        width: "275px", 
+        height: "150px",
+        margin: "20px",
+        textAlign: "center",
+    }
+
+    const frontStyle = {
+        borderRadius: "20px",
+        backgroundColor: "rgb(153, 186, 221)",
+        color: "white",
+        fontSize: "25px",
+        fontFamily: "sans-serif"
+    }
+
+    const backStyle = {
+        boxShadow: "4px 0.7px 20px 3px rgb(153, 186, 221)",
+        borderRadius: "20px",
+        color: "rgb(153, 186, 221)",
+        fontSize: "20px",
+        fontFamily: "sans-serif",
+        display: "flex",
+    }
+
+    const enlargeEmojiStyle = {
+        fontSize: "50px",
+        boxShadow: "4px 0.7px 20px 3px rgb(153, 186, 221)",
+        borderRadius: "20px",
+        color: "rgb(153, 186, 221)",
+    }
+
     const listItems = Object.entries(entry).map(([key, value]) => {
         return (
-            <div key = {key} className="display-entry-result">
-                <div className="flip-card-front">
-                    <h3>{key.charAt(0).toUpperCase() + key.slice(1)}</h3>
-                </div>
-                <div className="flip-card-back">
-                    <p>{value}</p>
-                </div>
+            <div key = {key}>
+                {value === entry.mood && key === "mood" ? 
+                    <Flippy key = {key}
+                    flipOnClick = {true}
+                    flipDirection = "horizontal"
+                    style = {cardStyle}
+                    > 
+                        <FrontSide style = {frontStyle}>   
+                            <h3>{key.charAt(0).toUpperCase() + key.slice(1)}</h3>
+                        </FrontSide>    
+    
+                        <BackSide style = {enlargeEmojiStyle}>
+                            <div className="vertical-align-emoji">{value}</div>
+                        </BackSide>
+                    </Flippy>
+                : 
+                    <Flippy key = {key}
+                    flipOnClick = {true}
+                    flipDirection = "horizontal"
+                    style = {cardStyle}
+                    > 
+                        <FrontSide style = {frontStyle}>   
+                            <h3>{key.charAt(0).toUpperCase() + key.slice(1)}</h3>
+                        </FrontSide>    
+
+                        <BackSide style = {backStyle}>
+                            <div className="vertical-align-text">{value}</div>
+                        </BackSide>
+                    </Flippy>
+                }    
             </div>
         )
     });
+
     const handleSubmitForSignIn = (e) => {
         e.preventDefault();
 
@@ -44,7 +101,9 @@ export default function StepSubmit(props) {
         if(localStorage.getItem('userid')){
             return (
                 <form onSubmit={handleSubmitForSignIn} >
-                    <button>Submit</button>
+                    <div className="button-container">
+                        <button className="wizard-button">Submit</button>
+                    </div>
                 </form>)
         } else {
             return (
