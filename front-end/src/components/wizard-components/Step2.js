1426c83b4c2175b7ac66bc948fcedf2368fe961c
Name Ping Louie <namepinglouie@gmail.com>
2022-05-28 12:56:34 -0400

    working on onClick event for wizard step2

--- front-end/src/components/wizard-components/Step2.js
+++ front-end/src/components/wizard-components/Step2.js
@@ -1,10 +1,5 @@
-import { useState } from "react"
-import hugIcon from "../../assets/icon-interests/hug.png"
-import massageIcon from "../../assets/icon-interests/massage.png"
-import musicIcon from "../../assets/icon-interests/music.png"
-import paintIcon from "../../assets/icon-interests/paint.png"
-import petsIcon from "../../assets/icon-interests/pets.png"
-import yogaIcon from "../../assets/icon-interests/yoga.png"
+import { useState } from "react";
+import interestIcons from "../../data/interestIcons";
 
 export default function Step2(props) {
     const {progressBarComponent, entry, setEntry, next, back} = props;
@@ -12,18 +7,21 @@ export default function Step2(props) {
     
     const handleSelectInterest = (e) => {
         if(!isActiveClick) {
-            e.target.style.border = "8px solid white";
-            e.target.style.borderRadius = "50px";
+            // e.target.style.border = "8px solid white";
+            // e.target.style.borderRadius = "inherit";
+            e.target.style.width = "150px";
             setEntry({...entry, interest: e.target.alt});
             setIsActiveClick(!isActiveClick);
         } else {
-            e.target.style.border = "none";
-            e.target.style.borderRadius = "none";
+            e.target.style.width = "100px";
             setEntry({...entry, interest: ""});
             setIsActiveClick(!isActiveClick);
         }
-
     }
+
+    const displayInterestIcons = interestIcons.map(({id, imgSrc, alt}) => {
+        return <img className="interest-img" key = {id} src = {imgSrc} alt = {alt} onClick={(e)=> handleSelectInterest(e)} />
+    })
 
     return(
         <form className="parent-container">
@@ -35,12 +33,7 @@ export default function Step2(props) {
                 <p className="wizard-question">Tell me, what's your interest?</p>
 
                 <div className="interest-container">
-                    <img src={hugIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="hug" className="interest-hover" />
-                    <img src={massageIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="acupuncture" className="interest-hover" />
-                    <img src={musicIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="music" className="interest-hover" />
-                    <img src={paintIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="art" className="interest-hover" />
-                    <img src={petsIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="pet" className="interest-hover" />
-                    <img src={yogaIcon} style={{width:"100px"}} onClick={(e)=> handleSelectInterest(e)} alt="yoga" className="interest-hover" />
+                    {displayInterestIcons}
                 </div>
                 
                 <div className="button-container step">
