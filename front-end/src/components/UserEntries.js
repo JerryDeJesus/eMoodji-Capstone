3348946490323a74d7ab03c1307fb6a545e3c58a
Jerry De Jesus <jdejesus7@live.com>
2022-05-31 16:18:25 -0400

    added chart to display user entries page

--- front-end/src/components/UserEntries.js
+++ front-end/src/components/UserEntries.js
@@ -3,14 +3,16 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { format, parseISO } from "date-fns";
 import Flippy, {FrontSide, BackSide} from "react-flippy";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import {Doughnut} from "react-chartjs-2";
 
 const API = process.env.REACT_APP_API_URL;
 const activitiesData = require('../data/activities.json');
-
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function UserEntries (){
     const [userEntries, setUserEntries] = useState([]);
-    
+
     let {id} = useParams();
     let navigate = useNavigate();
     
@@ -48,11 +50,11 @@ export default function UserEntries (){
         fontFamily: "sans-serif",
         display: "flex",
     }
+    
 
     let displayUserEntries = userEntries.map((entry, index)=>{
         let formattedDate = format(parseISO(entry.date_created), "MM/dd/yyyy hh:mm aaaaa'm'");
         let linkToEntry = `/entries/${entry.id}`;
-
         let resourceLink = "";
         let resourceDescription = "";
         let resourceArray = entry.interest && entry.activity ? activitiesData[entry.interest] : null;
@@ -62,7 +64,6 @@ export default function UserEntries (){
                 resourceDescription = each.description;
             }
         }
-
         return(
             <div className='usEntri' key = {index} >
                 <Flippy 
@@ -88,22 +89,64 @@ export default function UserEntries (){
                         </div>
                     </BackSide>
                 </Flippy>
-                {/* <Link to={linkToEntry}>
-                    <h3>Created At: {formattedDate}</h3>
-                    <h3>Mood: {entry.mood}</h3>
-                    <h3>Interest: {entry.interest}</h3>
-                    <h3>Activity: {entry.activity}</h3>
-                </Link> */}
+                
                 <br />
             </div>
         )
     })
+
+    let emojis = [];
+    let emojiCount = {};
     
+    let emojiGrabber = userEntries.map((entry) => {
+        emojis.push(entry.mood);    
+    })
+
+    let emojiCounter = emojis.map((emojis) => {
+        for(let each of emojis){
+            if(emojiCount[each]){
+                emojiCount[each]++;
+            }else{
+                emojiCount[each] = 1;
+            }
+        }
+    })
+    let emojiNums = Object.values(emojiCount);
+
+    console.log(emojiCount);
+
+    let doughnutChart = 
+        <div id="doughnut-chart"><Doughnut data={{
+            labels:[...Object.keys(emojiCount)],
+            datasets:[
+                {
+                    label: '# of Entries',
+                    data: [...emojiNums],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        ],
+                    borderWidth: 1,
+                }
+            ],   
+        }}/></div>;
 
     return(
         <div className="sUE">
             {userEntries.length ? displayUserEntries : "No Entries"}
+            {doughnutChart}
         </div>
     )
 }
-
