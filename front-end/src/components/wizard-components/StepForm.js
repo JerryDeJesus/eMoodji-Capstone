559593cb0dbaaf65c784f42fadc62be01325fe55
Jerry De Jesus <jdejesus7@live.com>
2022-10-31 17:13:21 -0400

    changed styling on progress bar

--- front-end/src/components/wizard-components/StepForm.js
+++ front-end/src/components/wizard-components/StepForm.js
@@ -26,9 +26,9 @@ export default function StepForm() {
     const back = () => setCurrentStep(currentStep - 1);
 
     switch (currentStep) {
-        case 1: return <Step1 progressBarComponent = {<Progress type="circle" strokeWidth={6} width={200} percent={25} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'‍🤔', color: '#FC2F22'}}}/>} entry = {entry} setEntry = {setEntry} next = {next} />;
-        case 2: return <Step2 progressBarComponent = {<Progress type="circle" strokeWidth={6} width={200} percent={50} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'‍🙂', color: '#FC9E22'}}}/>} entry = {entry} setEntry = {setEntry} next = {next} back = {back} />;
-        case 3: return <Step3 progressBarComponent = {<Progress type="circle" strokeWidth={6} width={200} percent={75} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'‍😀', color: '#fce849'}}}/>} entry = {entry} setEntry = {setEntry} next = {next} back = {back} activitiesData = {activitiesData} />;
-        default: return <Step4 progressBarComponent = {<Progress type="circle" strokeWidth={6} width={200} percent={100} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'😁', color: '#32C20E'}}}/>} entry = {entry} back = {back} activitiesData = {activitiesData} />;
+        case 1: return <Step1 progressBarComponent = {<Progress type="circle" strokeWidth={26} width={200} percent={25} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'‍🤔', color: '#FC2F22', trailColor: '#EFEFEF'}}}/>} entry = {entry} setEntry = {setEntry} next = {next} />;
+        case 2: return <Step2 progressBarComponent = {<Progress type="circle" strokeWidth={26} width={200} percent={50} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'‍🙂', color: '#FC9E22', trailColor: '#EFEFEF'}}}/>} entry = {entry} setEntry = {setEntry} next = {next} back = {back} />;
+        case 3: return <Step3 progressBarComponent = {<Progress type="circle" strokeWidth={26} width={200} percent={72} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'‍😀', color: '#FCE849', trailColor: '#EFEFEF'}}}/>} entry = {entry} setEntry = {setEntry} next = {next} back = {back} activitiesData = {activitiesData} />;
+        default: return <Step4 progressBarComponent = {<Progress type="circle" strokeWidth={26} width={200} percent={100} status="active" style={{fontSize: '100px'}} theme={{active: {symbol:'😁', color: '#32C20E', trailColor: '#EFEFEF'}}}/>} entry = {entry} back = {back} activitiesData = {activitiesData} />;
     }
 }
